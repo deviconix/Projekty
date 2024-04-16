ec134aef91497756136727e8309748c56332fdc2
Yuriy <emailworking@ukr.net>
2024-04-16 23:57:31 +0300

    created registrationController

---
+++ 1-projekt-backend-rejestracja/app/controllers/registrationController.js
@@ -0,0 +1,16 @@
+//const registrationService = require('../services/registrationService');
+
+const index = async (req, res) => {
+    try {
+        //        const applications = await registrationService.getAllApplications();
+        const applications = '<p>Please wait...</p>'
+        res.send(applications);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Ошибка сервера');
+    }
+};
+
+module.exports = {
+    index
+};
--- 1-projekt-backend-rejestracja/app/router/registrationRouter.js
+++ 1-projekt-backend-rejestracja/app/router/registrationRouter.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const registrationController = require('../controllers/registrationController');
+// router.get('/', function (req, res) {
+//     res.send('Router index tset');
+// });
 
-router.get('/', function (req, res) {
-    res.send('Router index tset');
-});
+router.get('/', registrationController.index);
 
 module.exports = router;
