a24b117e320ee0b019e8f58c420702f251d8946a
Yuriy <emailworking@ukr.net>
2024-04-18 19:58:05 +0300

    expressService : use body-server

--- 1-projekt-backend-rejestracja/app/services/expressService.js
+++ 1-projekt-backend-rejestracja/app/services/expressService.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const hbs = require('express-handlebars');
 
+const bodyParser = require('body-parser');
+
 function configureExpress() {
     const app = express();
 
     // Init handlebars
     app.engine("hbs", hbs.engine({ extname: ".hbs" }));
     app.set("view engine", "hbs");
-
+    // init body-parser
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
     return app;
 }
 
